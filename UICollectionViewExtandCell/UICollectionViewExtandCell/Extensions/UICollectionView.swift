1652d945e48b42762ae8ff1d4d622815fa0dae44
Jmy <didwlsah9@gmail.com>
2021-05-24 17:46:42 +0900

    Add ReusableView in UICollectionView Extension

--- UICollectionViewExtandCell/UICollectionViewExtandCell/Extensions/UICollectionView.swift
+++ UICollectionViewExtandCell/UICollectionViewExtandCell/Extensions/UICollectionView.swift
@@ -12,20 +12,34 @@ extension UICollectionView {
     // MARK: - Methods
     
     func register<T: UICollectionViewCell>(_: T.Type) {
-        register(T.self, forCellWithReuseIdentifier: T.description)
+        register(T.self, forCellWithReuseIdentifier: T.reuseIdentifier)
     }
     
     func registerNib<T: UICollectionViewCell>(_: T.Type) {
         let bundle: Bundle = Bundle(for: T.self)
-        let nib: UINib = UINib(nibName: T.description, bundle: bundle)
+        let nib: UINib = UINib(nibName: T.reuseIdentifier, bundle: bundle)
         
-        register(nib, forCellWithReuseIdentifier: T.description)
+        register(nib, forCellWithReuseIdentifier: T.reuseIdentifier)
     }
     
     func dequeueReusableCell<T: UICollectionViewCell>(for indexPath: IndexPath) -> T {
-        guard let cell = dequeueReusableCell(withReuseIdentifier: T.description, for: indexPath) as? T else {
-            fatalError("Could not dequeue cell: \(T.self) with identifier: \(T.description)")
+        guard let cell = dequeueReusableCell(withReuseIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
+            fatalError("Could not dequeue cell: \(T.self) with identifier: \(T.reuseIdentifier)")
         }
         return cell
     }
+}
+
+protocol ReusableView: class {
+    static var reuseIdentifier: String { get }
+}
+
+extension ReusableView where Self: UIView {
+    static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
+
+extension UICollectionViewCell: ReusableView {
+    
 }
