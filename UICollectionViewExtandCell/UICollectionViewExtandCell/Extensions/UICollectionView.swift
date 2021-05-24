bc389920c17937a39e34e179598edf033bbc9168
Jmy <didwlsah9@gmail.com>
2021-05-25 02:51:40 +0900

    Add extension UICollectionReusableView

--- UICollectionViewExtandCell/UICollectionViewExtandCell/Extensions/UICollectionView.swift
+++ UICollectionViewExtandCell/UICollectionViewExtandCell/Extensions/UICollectionView.swift
@@ -9,7 +9,7 @@ import UIKit
 
 extension UICollectionView {
     
-    // MARK: - Methods
+    // MARK: UICollectionViewCell
     
     func register<T: UICollectionViewCell>(_: T.Type) {
         register(T.self, forCellWithReuseIdentifier: T.reuseIdentifier)
@@ -28,18 +28,43 @@ extension UICollectionView {
         }
         return cell
     }
-}
-
-protocol ReusableView: class {
-    static var reuseIdentifier: String { get }
-}
-
-extension ReusableView where Self: UIView {
-    static var reuseIdentifier: String {
-        return String(describing: self)
+    
+    // MARK: UICollectionReusableView
+    
+    func registerHeader<T: UICollectionReusableView>(_: T.Type) {
+        register(T.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: T.reuseIdentifier)
+    }
+    
+    func registerFooter<T: UICollectionReusableView>(_: T.Type) {
+        register(T.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: T.reuseIdentifier)
+    }
+    
+    func dequeueReusableViewHeader<T: UICollectionReusableView>(for indexPath: IndexPath) -> T {
+        guard let cell = dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
+            fatalError("Could not dequeue cell: \(T.self) with identifier: \(T.reuseIdentifier)")
+        }
+        return cell
+    }
+    
+    func dequeueReusableViewFooter<T: UICollectionReusableView>(for indexPath: IndexPath) -> T {
+        guard let cell = dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
+            fatalError("Could not dequeue cell: \(T.self) with identifier: \(T.reuseIdentifier)")
+        }
+        return cell
     }
 }
 
-extension UICollectionViewCell: ReusableView {
-    
+extension UICollectionReusableView {
+    static var reuseIdentifier: String {
+        return String(describing: Self.self)
+    }
+}
+
+extension UIEdgeInsets {
+    init(horizontal: CGFloat, vertical: CGFloat) {
+        self.init(top: vertical, left: horizontal, bottom: vertical, right: horizontal)
+    }
+    static func uniform(size: CGFloat) -> UIEdgeInsets {
+        return UIEdgeInsets(top: size, left: size, bottom: size, right: size)
+    }
 }
