0b2c3234bb303a034e79dc3aed54811cc5a8bf93
zzimss <didwlsah9@gmail.com>
2022-06-21 15:53:36 +0900

    Update CocoaPods to SPM

--- UICollectionView_Extandable/UICollectionView_Extandable/Sources/Delegates/SceneDelegate.swift
+++ UICollectionView_Extandable/UICollectionView_Extandable/Sources/Delegates/SceneDelegate.swift
@@ -18,7 +18,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window = UIWindow(windowScene: windowScene)
         window?.windowScene = windowScene
         window?.rootViewController = SearchViewController()
-        window?.backgroundColor = .systemBackground
         window?.makeKeyAndVisible()
     }
 
