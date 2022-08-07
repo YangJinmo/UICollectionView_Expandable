92f281572185bcadc3a7f5d4631511a83e8d932b
zzimss <didwlsah9@gmail.com>
2022-08-07 23:08:27 +0900

    Add setBottomShadow, hideShadow, setShadow functions, navigationBarHeight property

--- UICollectionView_Extandable/Sources/Extensions/UIView.swift
+++ UICollectionView_Extandable/Sources/Extensions/UIView.swift
@@ -8,7 +8,33 @@
 import UIKit
 
 extension UIView {
+    var navigationBarHeight: CGFloat {
+        return 56
+    }
+
     func addSubviews(_ views: UIView...) {
         views.forEach { addSubview($0) }
+    }
+
+    func setBottomShadow() {
+        setShadow(x: 0, y: 1.6, blur: 1.6, alpha: 0.04)
+    }
+
+    func hideShadow() {
+        layer.shadowOpacity = 0
+    }
+
+    func setShadow(
+        x: CGFloat,
+        y: CGFloat,
+        blur: CGFloat,
+        alpha: Float
+    ) {
+        layer.masksToBounds = false
+        // layer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: layer.cornerRadius).cgPath
+        layer.shadowOffset = CGSize(width: x, height: y)
+        layer.shadowRadius = blur
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = alpha
     }
 }
