4ae47ea8d18f72664cb70bbce30fd67029bc8830
zzimss <didwlsah9@gmail.com>
2022-12-18 15:08:08 +0900

    Add TabBarController

---
+++ UICollectionView_Extandable/Sources/ViewControllers/TabBarController.swift
@@ -0,0 +1,173 @@
+//
+//  TabBarController.swift
+//  UICollectionView_Extandable
+//
+//  Created by Jmy on 2022/12/18.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+    // MARK: - Constants
+
+    private enum Image {
+        static let house = UIImage(systemName: "house")
+        static let magnifyingglass = UIImage(systemName: "magnifyingglass")
+        static let bag = UIImage(systemName: "bag")
+        static let message = UIImage(systemName: "message")
+        static let person = UIImage(systemName: "person")
+    }
+
+    private enum Color {
+        static let viewBackgroundColor = UIColor.systemBackground
+        static let tabBarTintColor = UIColor.label
+        static let tabBarBackgroundColor = UIColor.systemBackground
+        static let topBorderBackgroundColor = UIColor.secondarySystemBackground.cgColor
+    }
+
+    private let observers: [NSNotification.Name: Selector] = [
+        // out
+        UIScene.willDeactivateNotification: #selector(willDeactivate),
+        UIApplication.willResignActiveNotification: #selector(willResignActive),
+
+        // in
+        UIScene.willEnterForegroundNotification: #selector(willEnterForeground),
+        UIScene.didActivateNotification: #selector(didActivate),
+        UIApplication.didBecomeActiveNotification: #selector(didBecomeActive),
+    ]
+
+    // MARK: - View Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = Color.viewBackgroundColor
+
+        setupTabBar()
+        setupViewControllers()
+
+        addObservers()
+    }
+
+    deinit {
+        Self.identifier.log()
+    }
+
+    // MARK: - Methods
+
+    private func setupTabBar() {
+//        tabBar.tintColor = Color.tabBarTintColor
+//        tabBar.backgroundColor = Color.tabBarBackgroundColor
+
+//        let topBorder = CALayer()
+//        topBorder.frame = CGRect(x: 0, y: 0, width: 1000, height: 1)
+//        topBorder.backgroundColor = Color.topBorderBackgroundColor
+//
+//        tabBar.layer.addSublayer(topBorder)
+//        tabBar.clipsToBounds = true
+
+        tabBar.shadowImage = UIImage()
+        tabBar.backgroundImage = UIImage()
+        tabBar.backgroundColor = Color.tabBarBackgroundColor
+        tabBar.tintColor = Color.tabBarTintColor
+        tabBar.roundCorners(corners: [.topLeft, .topRight], radius: 25)
+        tabBar.setShadow(x: 0, y: -2, blur: 2, alpha: 0.07)
+    }
+
+    private func setupViewControllers() {
+        viewControllers = [
+            BaseNavigationController(
+                rootViewController: TableViewController(),
+                tabBarItem: UITabBarItem(
+                    title: "Home",
+                    image: Image.house,
+                    selectedImage: Image.house
+                )
+            ),
+            BaseNavigationController(
+                rootViewController: TimerViewController(),
+                tabBarItem: UITabBarItem(
+                    title: "Search",
+                    image: Image.magnifyingglass,
+                    selectedImage: Image.magnifyingglass
+                )
+            ),
+            BaseNavigationController(
+                rootViewController: ScrollableStackViewController(),
+                tabBarItem: UITabBarItem(
+                    title: "Basket",
+                    image: Image.bag,
+                    selectedImage: Image.bag
+                )
+            ),
+            BaseNavigationController(
+                rootViewController: ReviewViewController(),
+                tabBarItem: UITabBarItem(
+                    title: "Review",
+                    image: Image.message,
+                    selectedImage: Image.message
+                )
+            ),
+            BaseNavigationController(
+                rootViewController: MyViewController(),
+                tabBarItem: UITabBarItem(
+                    title: "My",
+                    image: Image.person,
+                    selectedImage: Image.person
+                )
+            ),
+        ]
+
+        delegate = self
+    }
+
+    private func removeObservers() {
+        observers.forEach {
+            NotificationCenter.default.removeObserver(self, name: $0.0, object: nil)
+        }
+    }
+
+    private func addObservers() {
+        observers.forEach {
+            NotificationCenter.default.addObserver(self, selector: $0.1, name: $0.0, object: nil)
+        }
+    }
+
+    @objc private func willDeactivate(_ notification: Notification) {
+        notification.name.rawValue.description.log()
+    }
+
+    @objc private func willResignActive(_ notification: Notification) {
+        notification.name.rawValue.description.log()
+    }
+
+    @objc private func willEnterForeground(_ notification: Notification) {
+        notification.name.rawValue.description.log()
+    }
+
+    @objc private func didActivate(_ notification: Notification) {
+        notification.name.rawValue.description.log()
+    }
+
+    @objc private func didBecomeActive(_ notification: Notification) {
+        notification.name.rawValue.description.log()
+    }
+}
+
+// MARK: - UITabBarControllerDelegate
+
+extension TabBarController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        guard let selectIndex = tabBarController.viewControllers?.firstIndex(of: viewController) else {
+            return false
+        }
+
+        selectIndex.description.log("Tab: \(tabBarController.selectedIndex) to ")
+
+        return true
+    }
+
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        tabBarController.selectedIndex.description.log()
+    }
+}
